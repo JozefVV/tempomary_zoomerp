fa20e91646fd41769ac73b1c7e73407eaf9b995a
Jozef Vindiš <jozefvindis194@gmail.com>
2019-04-23 14:29:51 +0200

    Category seeder

---
+++ database/seeds/CategoryTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+
+class CategoryTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        foreach (range(0, 10) as $index) {
+            Category::create(['name'=>'category_n_'.$index]);
+        }
+    }
+}
